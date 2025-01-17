86a6a894df1e347580fa9d27100237157c6dd2a7
renegadelha <renegadelha@gmail.com>
2025-01-17 11:17:04 -0300

    first commit

---
+++ servidor.py
@@ -0,0 +1,19 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/verificar', methods=['POST'])
+def ver():
+    nome = request.form.get('nome')
+
+    if nome == 'mikelly':
+        return '<h1>voce é aluna da turma do 3 ano</h1>'
+    else:
+        return '<h1>voce nao é Mikelly</h1>'
+
+if __name__ == '__main__':
+    app.run()
